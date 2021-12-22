85a6283071d8e252f2992b4ed1324d9dd1990c77
Jaakko <jaakko.niininen@gmail.com>
2021-12-22 20:34:16 +0200

    Kaikki turhat poistettu, nyt rakentamaan pelkistettyä versiota sivuista

--- KokMet/src/Tapahtumat.js
+++ KokMet/src/Tapahtumat.js
@@ -1,20 +1,20 @@
 import './Main.css';
 import { Component } from "react";
-import Tapahtumalista from "./Tapahtumalista.js";
 import Logo from "./logo.png";
 
 class Tapahtumat extends Component {
   render() {
-    return <div className="Tapahtumat">
+    return <div className="Tapahtumat" >
       <div className="Header">
-        <header className="Otsikko">
-          <img src={Logo} width="170" height="50" alt="Teollisuusliiton logo" />
-          <h1>Metalli 67</h1>
+        <header className="Otsikko" >
+          <img src={Logo}
+            width="170"
+            height="50"
+            alt="Teollisuusliiton logo" />
+          <h1 > Metalli 67 </h1>
         </header>
       </div>
-      <div key="Tapahtumalista" className="Tapahtumalista"><Tapahtumalista /></div>
     </div>;
-
   }
 }
 
