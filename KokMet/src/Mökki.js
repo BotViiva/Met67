975e2fddf4a9bd28fe9584647ebc94e5b152828d
Jaakko <jaakko.niininen@gmail.com>
2021-12-22 23:32:10 +0200

    Mökki-osio riittävän hyvä, siirrytään jäsenkirjeeseen

--- KokMet/src/Mökki.js
+++ KokMet/src/Mökki.js
@@ -1,12 +1,25 @@
 import './Main.css';
+import MökkiKuva from './mokki.jpg'
 import { Component } from "react";
 
 class Mökki extends Component {
   render() {
     return <div className="Mökki">
-      <header className="Toinen">
-        <h1>Tää ois sitte toinen</h1>
-      </header>
+
+      <h1>Osaston Mökki</h1>
+      <img src={MökkiKuva} id="Kuva"></img>
+      <div class="mokkidiv">
+        <p>Osaston huvila sijaitsee Honkaluodossa osoitteessa Jopintie 108</p>
+        <p>Varaukset: Veikko Kiema</p>
+        <p>Yht. Tied.</p>
+        <p>veikko.kiema@gmail.com</p>
+        <p>0400 274862</p>
+        <p>Avainmaksu osaston jäsenille</p>
+        <p>su - to 30€, to - su 30€</p>
+        <p>Syyskuun ensimmäinen viikko su - su 30€</p>
+        <p>Osaston tilinumero: FI51 1065 3007 101235</p>
+        <p>Avainten palautus 15:00 ja nouto klo 18:00</p>
+      </div>
     </div>;
   }
 }
