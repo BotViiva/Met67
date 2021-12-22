2f42559b1753d22fbfd88a862edbe01d847ca7c2
Jaakko <jaakko.niininen@gmail.com>
2021-12-22 23:07:55 +0200

    Landing page tarpeeksi hyvä, siirrytään mökki-osioon

--- KokMet/src/Tapahtumat.js
+++ KokMet/src/Tapahtumat.js
@@ -7,12 +7,40 @@ class Tapahtumat extends Component {
     return <div className="Tapahtumat" >
       <div className="Header">
         <header className="Otsikko" >
-          <img src={Logo}
-            width="170"
-            height="50"
-            alt="Teollisuusliiton logo" />
-          <h1 > Metalli 67 </h1>
+          <a className="Logo" href="https://www.teollisuusliitto.fi/">
+            <img className="Logo-img" src={Logo}
+              width="170"
+              height="50"
+              alt="Teollisuusliiton logo" />
+          </a>
+          <div className="Pääotsikko">
+            <h1> Metalli 67 </h1>
+          </div>
         </header>
+      </div>
+      <div className="Tervetuloa">
+        <h2>
+          Tervetuloa Kokkolan metallityöväen ammattiosasto 67:n sivuille!
+        </h2>
+        <p>
+          Tällä sivulla ilmoitamme osaston tapahtumat sekä kokoukset.
+        </p>
+        <p>
+          Hallitus kokoontuu puheenjohtajan kutsusta 1- 2 kk. välein osaston toimistolla. Kevät- ja syyskokous ilmoitetaan
+          erikseen täällä sekä Teollisuusliiton sivuilla.
+        </p>
+      </div>
+      <div className="Container">
+        <section className="Item">
+          <h2>SEURAAVAT TAPAHTUMAT</h2>
+          <p>Kylpyläreissu Sanifaniin 29.1.2022</p>
+          <p>Koko perheen pilkkikisat 12.3.2022</p>
+        </section>
+        <section className="Item">
+          <h2>SEURAAVA KOKOUS</h2>
+          <p>Hallituksen kokous</p>
+          <p>14.2.2022 Klo 19:00</p>
+        </section>
       </div>
     </div>;
   }
