1b3616667aabf2af6f266a7c416bce7bbd4191b2
Jaakko <jaakko.niininen@gmail.com>
2024-01-12 14:47:55 +0200

    Kuvat siirretty omaan kansioon, kuvagalleria toimii.

--- KokMet/src/index.js
+++ KokMet/src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 import Tapahtumat from './Tapahtumat';
 import Mökki from './Mökki';
 import Hallitus from './Hallitus';
+import Galleria from './Galleria';
 
 ReactDOM.render(
   <React.StrictMode>
     <Tapahtumat />
     <Hallitus />
     <Mökki />
+    <Galleria />
   </React.StrictMode>,
   document.getElementById('root')
 );
