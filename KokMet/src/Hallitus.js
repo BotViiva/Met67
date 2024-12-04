1efaa549f0a8ed0c7bd8abc598734740bcfaf668
Jaakko <jaakko.niininen@gmail.com>
2024-12-04 20:23:23 +0200

    Lisätty seuraavan vuoden jäsenet, sekä tuleva tapahtuma

--- KokMet/src/Hallitus.js
+++ KokMet/src/Hallitus.js
@@ -1,6 +1,6 @@
 import "./Main.css";
 import { Component } from "react";
-import Esite from "./Tiedostot/Vaasa2024.pdf";
+import Esite from "./Tiedostot/Kalajoki2025.pdf";
 import EsiteTre from "./Tiedostot/TAMPERE.pdf";
 import Saunailta from "./Tiedostot/Saunailta.pdf";
 import HallitusLista from "./HallituksenLuonti.js";
@@ -10,11 +10,11 @@ class Hallitus extends Component {
     return (
       <div className="Hallitus">
         <div className="Jäsenkirje">
-          <h2>Tulevat tapahtumat vuodelle 2024</h2>
-          <h3>Kylpyläreissu Tropiclandiaan 20.1.2024</h3>
+          <h2>Tulevat tapahtumat</h2>
+          <h3>Kylpyläreissu Sanifaniin 8.2.2025</h3>
           <p>
-            Kokkolan metallityöväen ao.67:n perinteinen koko perheen
-            kylpylämatka tehdään Tropiclandiaan Vaasaan.
+          Kokkolan metallityöväen ao.67:n perinteinen koko perheen
+          kylpylämatka tehdään Kalajoen Sanifaniin.
           </p>
           <a href={Esite} target="_blank" rel="noreferrer">
             Lataa esite
@@ -47,7 +47,7 @@ class Hallitus extends Component {
             Kaikki omavastuut maksetaan ennen tapahtumaa osaston tilinumeroon
             FI51 1065 3007 1012 35
           </h2>
-          <h2>Hallitus ja toimihenkilöt vuodelle 2024</h2>
+          <h2>Hallitus ja toimihenkilöt vuodelle 2025</h2>
           <div className="Henkilöt">
             <HallitusLista />
           </div>
