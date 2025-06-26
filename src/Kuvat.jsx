683e315d11ac06449df3d31c5d2dd0e2cabe690b
Jaakko <jaakko.niininen@gmail.com>
2025-06-26 07:37:53 +0300

    Mökin kuvat uusittu

--- src/Kuvat.jsx
+++ src/Kuvat.jsx
@@ -1,9 +1,15 @@
-import Mokki from './Kuvat/uusimokki.jpg'
-import Keittio1 from './Kuvat/uusikeittio1.jpg'
-import Keittio2 from './Kuvat/uusikeittio2.jpg'
-import Piha from './Kuvat/uusipiha.jpg'
-import Sanky from './Kuvat/uusisanky.jpg'
-import Sauna from './Kuvat/uusisauna.jpg'
+import Mokki from './Kuvat/1.jpg'
+import Mokkisisalta1 from './Kuvat/2.jpg'
+import Mokkisisalta2 from './Kuvat/3.jpg'
+import Mokkisisalta3 from './Kuvat/4.jpg'
+import Mokkisisalta4 from './Kuvat/5.jpg'
+import Piha from './Kuvat/6.jpg'
+import Grillikota from './Kuvat/7.jpg'
+import Sauna from './Kuvat/8.jpg'
+import Laituri from './Kuvat/9.jpg'
+import Pikkumokki from './Kuvat/10.jpg'
+import Pikkumokkisisalta from './Kuvat/11.jpg'
+
 
 const images = [
     {
@@ -11,24 +17,44 @@ const images = [
         thumbnail: Mokki,
     },
     {
-        original: Keittio1,
-        thumbnail: Keittio1,
+        original: Mokkisisalta1,
+        thumbnail: Mokkisisalta1,
     },
     {
-        original: Keittio2,
-        thumbnail: Keittio2,
+        original: Mokkisisalta2,
+        thumbnail: Mokkisisalta2,
+    },
+    {
+        original: Mokkisisalta3,
+        thumbnail: Mokkisisalta3,
+    },
+    {
+        original: Mokkisisalta4,
+        thumbnail: Mokkisisalta4,
     },
     {
         original: Piha,
         thumbnail: Piha,
     },
     {
-        original: Sanky,
-        thumbnail: Sanky,
+        original: Grillikota,
+        thumbnail: Grillikota,
     },
     {
         original: Sauna,
         thumbnail: Sauna,
+    },
+    {
+        original: Laituri,
+        thumbnail:  Laituri,
+    },
+    {
+        original: Pikkumokki,
+        thumbnail:  Pikkumokki,
+    },
+    {
+        original: Pikkumokkisisalta,
+        thumbnail: Pikkumokkisisalta,
     },
 
 ];
